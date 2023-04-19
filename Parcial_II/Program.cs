1616fb2d658f712d0c77cdb3953c4a60c6ba237e
EliasO23 <124007307+EliasO23@users.noreply.github.com>
2023-04-18 20:51:07 -0600

    Actualizacion

---
+++ Parcial_II/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Codigo con error
+
+//int datol = 16;
+//String dato2 = "25g";
+//int resultado = datol + Convert.ToInt32(dato2);
+
+//Codigo reparado con la sintaxis “try{} catch{}”
+
+try
+{
+    int dato1 = 16;
+    string dato2 = "25g";
+    int resultado = dato1 + Convert.ToInt32(dato2);
+}
+catch
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
+
+
+
